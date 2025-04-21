da65c703c825b149eeb6c28a96c0aecb67fb53e7
ummesalma303 <ummesalma333999@gmail.com>
2025-04-21 13:24:02 +0600

    added sign in form

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,9 +1,13 @@
+// import { SignUpForm } from '@/components/auth/SignUpForm'
+import SignInForm from '@/components/SignInForm'
 import React from 'react'
 
 const SignIn = () => {
   return (
-    <div>
-      
+    <div className=" flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <SignInForm />
+      </div>
     </div>
   )
 }
