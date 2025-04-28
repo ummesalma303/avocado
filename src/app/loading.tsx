dfff427300f32a6ffdf2f4999c2cb59087d8716a
ummesalma303 <ummesalma333999@gmail.com>
2025-04-28 16:46:05 +0600

    Added loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
+// import React from 'react'
+import {TrophySpin} from "react-loading-indicators"
 const loading = () => {
   return (
-    <div>loading</div>
+    <div className="w-full h-screen flex justify-center items-center">
+        <TrophySpin color={["#32cd32", "#327fcd", "#cd32cd", "#cd8032"]} text="loading..." textColor="#6c4141" />
+        {/* <Atom color="#0b124a" size="small"  /> */}
+    </div>
   )
 }
 
